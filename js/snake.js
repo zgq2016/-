29af48ae2b831982067658e4deb7966ca47ee7e0
zgq <zgq>
2020-03-10 23:09:32 +0800

    snake move完成

---
+++ js/snake.js
@@ -0,0 +1,53 @@
+(function() {
+  function Snake(obj) {
+    obj = obj || {};
+    this.width = obj.width || 20;
+    this.height = obj.height || 20;
+    this.body = obj.body || [
+      {
+        left: 3 * this.width,
+        top: 1 * this.height,
+        backgroundColor: "#fff"
+      },
+      {
+        left: 2 * this.width,
+        top: 1 * this.height,
+        backgroundColor: "#000"
+      },
+      {
+        left: 1 * this.width,
+        top: 1 * this.height,
+        backgroundColor: "red"
+      }
+    ];
+    this.element = [];
+    this.direction = obj.direction || "right";
+    this.render();
+  }
+  Snake.prototype.render = function() {
+    for (let i = 0; i < this.body.length; i++) {
+      this.element[i] = document.createElement("div");
+      map.element.appendChild(this.element[i]);
+    }
+    this.style();
+  };
+  Snake.prototype.style = function() {
+    for (let i = 0; i < this.body.length; i++) {
+      this.element[i].style.width = this.width + "px";
+      this.element[i].style.height = this.height + "px";
+      this.element[i].style.backgroundColor = this.body[i].backgroundColor;
+      this.element[i].style.position = "absolute";
+      this.element[i].style.left = this.body[i].left + "px";
+      this.element[i].style.top = this.body[i].top + "px";
+    }
+  };
+  Snake.prototype.move = function() {
+    for (let i = this.body.length - 1; i > 0; i--) {
+      this.body[i].left = this.body[i - 1].left;
+      this.body[i].top = this.body[i - 1].top;
+    }
+    this.body[0].left += this.width;
+    this.style();
+  };
+  window.Snake = Snake;
+})();
